4e154a245f8df3ea74303a13d398f17633e43904
Danny <93397389+Dcastro99@users.noreply.github.com>
2023-01-26 20:44:36 -0800

    new component

--- app/javascript/react/src/components/Welcome.jsx
+++ app/javascript/react/src/components/Welcome.jsx
@@ -1,12 +1,16 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
+import QuestionList from './questionList';
 
 const Welcome = () => {
 return(
+  <>
 <div className='container'>
 <h1>Welcome to my Rails 7 App!!</h1>
 <p>Here we go. Going to expand on this.</p>
 </div>
+<QuestionList />
+</>
 )
 }
 
--- app/javascript/react/src/components/questionList.jsx
+++ app/javascript/react/src/components/questionList.jsx
@@ -46,4 +46,23 @@ const QuestionList = () => {
 
   ]
 
+  return(
+    <div className='row'>
+      <div className='col-lg-10 mx-auto'>
+        {questionsList.map((question) => 
+        // eslint-disable-next-line react/jsx-key
+        <div className='card rounded-0 mt-3'>
+          <div className='card-body'>
+            <h3 className='card-title'>{question.title}</h3>
+            <p className='lead'>
+              <span className='badge bg-primary'>{question.tag}</span>
+            </p>
+          </div>
+        </div>
+        )}
+      </div>
+    </div>
+  )
 }
+
+export default QuestionList
